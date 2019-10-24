bd3d4de42426ba92168e3e9591892a0297f0b0ba
Tim Ogden <togden1999@gmail.com>
2019-10-23 21:11:16 -0400

    Working on tensorflow version, trying to get tensor math right.

--- tf_colortimeline.py
+++ tf_colortimeline.py
@@ -2,8 +2,9 @@ import tensorflow as tf
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 filename = 'buttercup.mp4'
-batch_size = 64
+batch_size = 128
 
 def count_frames(path, override=False):
 	# grab a pointer to the video file and initialize the total
@@ -22,10 +23,14 @@ def count_frames(path, override=False):
 	return total
 
 X = tf.placeholder(tf.float32, name='X')
-kr = tf.reduce_mean(X[:,:,:,2])
-kg = tf.reduce_mean(X[:,:,:,1])
-kb = tf.reduce_mean(X[:,:,:,0])
-y = tf.cast(tf.stack([kr, kg, kb], axis=0), tf.int16)
+
+flat_x = tf.reshape(X, (-1,360*640,3))
+kr = tf.reduce_mean(X[:,:,2], 0) # batch num, pixel, rgb channel
+kg = tf.reduce_mean(X[:,:,1], 0)
+kb = tf.reduce_mean(X[:,:,0], 0)
+#y = tf.cast(tf.stack([kr, kg, kb], axis=-1), tf.int16)
+y = tf.cast(tf.reduce_mean(X, 0), tf.int16)
+rank = tf.rank(y)
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
@@ -35,16 +40,40 @@ with tf.Session() as sess:
 	cap = cv2.VideoCapture(filename)
 	frames = []
 	outputs = []
-	while(True):
-		ret, frame = cap.read()
-		if not ret:
-			break
-		frames.append(frame)
-		if len(frames) % batch_size == 0:
-			outputs.append(y.eval(feed_dict={X: frames}))
-			frames = []
+	num_batches = int(np.ceil(num_frames/batch_size))
+	print(num_batches)
+	batch = 0
+	i = 0
+	for batch in tqdm(range(num_batches)):
+		for k in range(batch_size):
+			ret, frame = cap.read()
+			i+=1
+			if not ret:
+				break
+			frames.append(frame)
+			if len(frames) % batch_size == 0:
+				batch+=1
+				#print('Rank:', rank.eval(feed_dict={X: frames}))
+				#print('Batch: {} of {}'.format(batch, num_batches))
+				#print(rank.eval(feed_dict={X: frames}))
+				result = y.eval(feed_dict={X: frames})
+				
+				outputs.append([r for r in result])
+				#print(result.shape)
+				frames = []
 	outputs.append(y.eval(feed_dict={X: frames}))
-	picture = np.array([outputs for x in range(int(len(outputs)*.3))])
+	outputs = np.array(outputs)
+	picture = []
+	print('Num frames read:', i)
+	print(outputs.shape)
+	#for batch in outputs:
+	#	picture.append([x[0],x[1],x[2]])
+	print('Turning array into two dimensional picture...')
+	print(len(picture))
+	#picture = np.array([picture for x in tqdm(range(int(.3*num_frames)))])
+	#print('Give the program a second as matplotlib loads the image...')
+	
 	plt.imshow(picture)
+	
 	plt.show()
 cap.release()
